e494b80feea057da987f0610c1aa085a715cdee0
DJaegerScript <adjiedjaka0902@gmail.com>
2023-05-07 14:45:06 +0700

    created authentication page

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from authentication.views import show_login_page
+from authentication.views import show_login_page, show_registration_page
 
 app_name = 'authentication'
 
 urlpatterns = [
-    path('', show_login_page, name='show_login_page'),
+    path('login', show_login_page, name='show_login_page'),
+    path('registration', show_registration_page, name='show_registration_page'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def show_login_page(request):
     return render(request, 'login.html')
+
+def show_registration_page(request):
+    return render(request, 'registration.html')
