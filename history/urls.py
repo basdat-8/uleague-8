eba7b35c601202a4a6f2b57369818a76ff05223b
rafliwasis <rafli.wasis@ui.ac.id>
2023-05-23 10:58:44 +0700

    update

--- history/urls.py
+++ history/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_rapat, name='list_rapat'),
+    path('history', views.list_rapat, name='list_rapat'),
     path('laporan/<int:rapat_id>/', views.lihat_laporan, name='lihat_laporan'),
 ]
