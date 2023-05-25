7f4adc6cd0808329bba3dcedd073bed150eebfdb
DJaegerScript <adjiedjaka0902@gmail.com>
2023-05-25 23:10:38 +0700

    added csrf trusted origins

--- uleague_8/settings.py
+++ uleague_8/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = 'django-insecure-3@5wx%(^*zl68l(o$^m-3%cvjv0g&mom1ra=oj5f048_al57s7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['uleague-8-production.up.railway.app']
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['https://uleague-8-production.up.railway.app','https://*.127.0.0.1']
 
 # Application definition
 
